4ce82ed9b354b4eb1ad55d7ebfb47a2e8edb54bd
JofinJohnson <jofinjohnson.p@gmail.com>
2023-08-05 13:03:32 +0530

    Initialize Spring Boot Project

---
+++ web-flux-review/src/main/java/com/springreview/webfluxreview/WebFluxReviewApplication.java
@@ -0,0 +1,13 @@
+package com.springreview.webfluxreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFluxReviewApplication.class, args);
+	}
+
+}
---
+++ web-flux-review/src/test/java/com/springreview/webfluxreview/WebFluxReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springreview.webfluxreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
